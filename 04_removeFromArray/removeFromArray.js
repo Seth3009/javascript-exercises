613ec56c1c302d27f836c42fed709185e388ab38
Setiadi <setiadi.seth@gmail.com>
2022-05-26 21:32:15 +0700

    Fourth test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+    let arg = [...args];
+    for (let i = 0; i < arg.length; i++){
+        if(arr.indexOf(arg[i]) > -1){
+            arr.splice(arr.indexOf(arg[i]),1)
+        }
+    } 
+    return arr
 };
 
 // Do not edit below this line
