7f29a75e1d808f209273f719878cd43fd560977c
Setiadi <setiadi.seth@gmail.com>
2022-05-26 22:06:23 +0700

    Fifth test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    
+    if(typeof a === "number" && typeof b === "number" && a > 0 && b > 0){
+        let lowest = Math.min(a,b);
+        let highest = Math.max(a,b);
+        let sumall = 0;
+        for(let i=lowest; i <= highest; i++) {
+            sumall += i;
+        }
+        return sumall;
+    } else { return 'ERROR'}
 };
 
 // Do not edit below this line
