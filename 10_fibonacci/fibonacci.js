63c6bc2b61eb630f0dc11d3ce339ef12cf0cc8dc
Setiadi <setiadi.seth@gmail.com>
2022-06-06 22:48:55 +0700

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    if(count === 0) return 0;
 
+    let before = 0;
+	let actual = 1;
+	let next;
+
+	for(let i = 1; i < count; i++){
+		next = actual;
+        actual = before + actual;
+		before = next;
+	}
+    return actual;
 };
 
 // Do not edit below this line
